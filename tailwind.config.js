09b25bf2d95c2b1b2cc00e534a1492946cb5bb95
marinabel <marina.bellair@gmail.com>
2025-12-10 21:41:27 +0100

    feat: Migrate Tailwind CSS to PostCSS (production setup)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Cible tous les fichiers de code source React/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
